9e0f8f784d73e07852374992133bb57993f54a68
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-07-21 17:01:34 +0100

    Add get book stock and add book method to library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+
+    public Library(){
+        this.books = new ArrayList<Book>();
+    }
+
+    public int bookStock(){
+        return this.books.size();
+    }
+
+    public void addBook(Book book){
+        this.books.add(book);
+    }
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+    private Book book;
+
+    @Before
+    public void before(){
+        library = new Library();
+        book = new Book("Harry Potter", "Rowling", "Fiction");
+        library.addBook(book);
+    }
+
+    @Test
+    public void hasBooks(){
+        assertEquals(1, library.bookStock());
+    }
+
+    @Test
+    public void canAddBook(){
+        library.addBook(book);
+        assertEquals(2, library.bookStock());
+    }
 }
